56b3dc7e1c345304436582f020112db9cfee3650
ssvyatoss <107925873+ssvyatoss@users.noreply.github.com>
2022-09-12 15:27:50 +0000

    Deploying to gh-pages from @ ssvyatoss/goit-js-hw-09@f6554290bb082f0ec7aef7c1930c875b4dd51814 🚀

---
+++ 01-color-switcher.1a5edfbc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(){e.disabled=!0,o=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.1a5edfbc.js.map
---
+++ 01-color-switcher.27ac0259.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");let d=null;e.addEventListener("click",(()=>{e.disabled=!0,d=setInterval((()=>{t.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),a.addEventListener("click",(()=>{e.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.27ac0259.js.map
